6b13d530222eacc056e80859ab32cd3501feacbb
Alexi Lallas Ribeiro Pereira <alexii2005@hotmail.com>
2016-10-16 02:13:46 -0200

    Add files via upload

---
+++ PlotGraphic.cpp
@@ -0,0 +1,66 @@
+#include <GL/glut.h>
+#include <stdlib.h>
+#include <math.h>
+#include "graphics.h"
+#define pi 3.1415
+
+// prototipos das funcoes
+void init(void);
+void draw(void);
+void keyboard(unsigned char key, int x, int y);
+
+// funcao principal
+int main(int argc, char** argv) {
+
+	glutInit(&argc, argv);                              // inicializa o glut
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);           // especifica o uso de cores e buffers
+	glutInitWindowSize(800, 600);                          // especifica as dimensoes da janela
+	glutInitWindowPosition(283, 64);                      // especifica aonde a janela aparece na tela
+	glutCreateWindow("Desenhando uma Reta");              // cria a janela
+	init();
+	glutDisplayFunc(draw);                               // funcao que sera redesenhada pelo GLUT
+	glutKeyboardFunc(keyboard);                             // funcoes de teclado
+	glutMainLoop();                                         // mostra todas as janelas criadas
+	return 0;
+}
+
+// definicao de cada funcao
+
+void init(void) {
+	glClearColor(1.0, 1.0, 1.0, 1.0);    // cor de fundo
+	gluOrtho2D(-100, 100, -100, 100);     // modo de projecao ortogonal
+}
+
+
+void draw(void) {
+	glClear(GL_COLOR_BUFFER_BIT);               // limpa a janela
+	glColor3f(1.0, 0.0, 0.0);                  // cor da linha
+	glBegin(GL_POINTS);
+
+	//desenho Reta
+	
+	int ponto1[2] = { 0, 100 };
+	int ponto2[2] = { 100, 0 };
+
+	float x;
+	float y;
+	float m;
+	m = (ponto2[1] - ponto1[1]) / (ponto2[0] - ponto1[0]);
+
+	for (x = -100; x <= 100; x+=0.0001) {
+		//y = 10*sin(x/10);
+		y = 1/(x);
+		glVertex2f(x, y);
+	}
+			
+	glEnd();
+	glFlush();
+}
+
+void keyboard(unsigned char key, int x, int y) {
+	switch (key) {
+	case 27:                                         // tecla Esc (encerra o programa)
+		exit(0);
+		break;
+	}
+}
